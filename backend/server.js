b6c65e23a9309dbb6821daf7a73a62db72259fda
kshitij2201 <kshitijmeshram31@gmail.com>
2025-05-23 13:01:44 +0530

    new rec

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ import reportRoutes from './routes/reportRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Get the directory name using ES module
